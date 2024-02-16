1bfd4087025d3bf44d0824f9a8aa16458692813a
Bruce <recall4056@gmail.com>
2024-02-17 01:20:36 +0800

    fix: :bug: 登录成功添加提示信息

--- apps/admin/src/features/login/mutations/useLoginMutation.ts
+++ apps/admin/src/features/login/mutations/useLoginMutation.ts
@@ -1,6 +1,9 @@
 import type { LoginDto } from '@/api/auth.type'
 
-export const useLoginMutation = () =>
-  useMutation({
-    mutationFn: (data: LoginDto) => AuthAPI.login(data)
+export const useLoginMutation = () => {
+  const { message } = AApp.useApp()
+  return useMutation({
+    mutationFn: (data: LoginDto) => AuthAPI.login(data),
+    onSuccess: () => message.success('登录成功')
   })
+}
