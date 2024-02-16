7077f7f6f0bbf194127b12f38ea8debe8d1e8a17
Bruce <recall4056@gmail.com>
2024-02-16 11:22:33 +0800

    chore: :hammer: 添加 components 文件夹自动引入

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -76,7 +76,7 @@ export default defineConfig(({ mode }) => {
           AhooksResolver(),
           RaipiotAntdResolver()
         ],
-        dirs: ['src/api/**', 'src/hooks/**', 'src/store/**']
+        dirs: ['src/api/**', 'src/components/**', 'src/hooks/**', 'src/store/**']
       }),
       BootstrapAnimation({
         name: 'SRM Admin',
