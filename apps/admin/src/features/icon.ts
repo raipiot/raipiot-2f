5ca9557aa1fd4b349b86e6de6a9ecfceedff4c6c
Bruce <recall4056@gmail.com>
2024-02-18 09:51:01 +0800

    style: :lipstick: 修改执行脚本错别字

---
+++ apps/admin/src/features/icon.ts
@@ -0,0 +1,49 @@
+import TwoCol from '~icons/carbon/open-panel-left'
+import Settings from '~icons/heroicons/cog-6-tooth'
+import Check from '~icons/ic/round-check'
+import Lang from '~icons/ion/language-outline'
+import ChevronDoubleLeft from '~icons/line-md/chevron-small-double-left'
+import Docs from '~icons/line-md/document-list'
+import Loading from '~icons/line-md/loading-twotone-loop'
+import React from '~icons/logos/react'
+import Account from '~icons/material-symbols/account-circle'
+import Add from '~icons/material-symbols/add-rounded'
+import Table from '~icons/material-symbols/backup-table-rounded'
+import Close from '~icons/material-symbols/close-rounded'
+import Code from '~icons/material-symbols/code-rounded'
+import Copy from '~icons/material-symbols/content-copy-outline-rounded'
+import Error from '~icons/material-symbols/error-outline'
+import Key from '~icons/material-symbols/key-vertical-outline-rounded'
+import Books from '~icons/material-symbols/library-books-outline-rounded'
+import Lock from '~icons/material-symbols/lock-outline'
+import Dictionary from '~icons/material-symbols/menu-book-outline-rounded'
+import Menu from '~icons/material-symbols/menu-rounded'
+import Package from '~icons/material-symbols/package-2-outline'
+import Refresh from '~icons/material-symbols/refresh-rounded'
+import Tools from '~icons/material-symbols/tools-wrench-outline'
+
+export const iconSet = {
+  React,
+  Loading,
+  Lang,
+  Refresh,
+  Settings,
+  Tools,
+  Menu,
+  Key,
+  Account,
+  Lock,
+  Error,
+  Code,
+  Table,
+  Dictionary,
+  Books,
+  TwoCol,
+  Check,
+  Close,
+  Docs,
+  Add,
+  Copy,
+  Package,
+  'Chevron:Double:Left': ChevronDoubleLeft
+}
