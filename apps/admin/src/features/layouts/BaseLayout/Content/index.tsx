1565f123022bee4c485431455dc7ff3438ff9e38
Developer27149 <564151947@qq.com>
2024-03-04 16:29:29 +0800

    chore: :hammer: eslint 问题

--- apps/admin/src/features/layouts/BaseLayout/Content/index.tsx
+++ apps/admin/src/features/layouts/BaseLayout/Content/index.tsx
@@ -4,7 +4,7 @@ import { BaseLayoutTransition } from './BaseLayoutTransition'
 export default function Content() {
   return (
     <ALayout.Content>
-      <BaseLayoutTransition className="relative h-[calc(100vh-136px)] overflow-y-scroll p-2 sm:p-4 bg-white dark:bg-dark/80">
+      <BaseLayoutTransition className="relative h-[calc(100vh-136px)] overflow-y-scroll bg-white p-2 dark:bg-dark/80 sm:p-4">
         <Outlet />
       </BaseLayoutTransition>
     </ALayout.Content>
