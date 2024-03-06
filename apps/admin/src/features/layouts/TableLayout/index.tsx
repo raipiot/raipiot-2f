c61ddd0199c69a2f324bbc9e66afdb9a9e46729c
Bruce <recall4056@gmail.com>
2024-03-06 22:29:28 +0800

    style: :lipstick: 调整类型引入

--- apps/admin/src/features/layouts/TableLayout/index.tsx
+++ apps/admin/src/features/layouts/TableLayout/index.tsx
@@ -1,5 +1,5 @@
 import type { ModalProps } from 'antd'
-import { type HTMLAttributes, type PropsWithChildren, type ReactNode } from 'react'
+import type { HTMLAttributes, PropsWithChildren, ReactNode } from 'react'
 
 import type { RpSearchBarProps } from '@/shared/components/RpSearchBar'
 
