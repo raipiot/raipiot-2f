18e2113e421231c560f778637394b6055a6d4933
Bruce <recall4056@gmail.com>
2024-03-07 12:37:14 +0800

    chore: :hammer: 同步 infra

--- apps/admin/src/features/containers/PageContainer/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { HTMLAttributes } from 'react'
-
-interface PageContainerProps {
-  rootProps?: HTMLAttributes<HTMLDivElement>
-}
-
-export function PageContainer() {
-  return <div>123</div>
-}
--- packages/hooks/README.md
+++
@@ -1,5 +0,0 @@
-# @raipiot-2f/hooks
-
-raipiot 2f 通用 Hooks。
-
-aHooks 部分 Hooks 从 `index.ts` 导出。
