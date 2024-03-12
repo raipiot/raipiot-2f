715124ad5c3b4bd85aa294e8e62d5bf8cfaaf582
Bruce <recall4056@gmail.com>
2024-03-12 17:15:03 +0800

    feat(api,i18n): :sparkles: 租户管理接口定义、多语言

---
+++ packages/api/src/system/tenant/dto.ts
@@ -0,0 +1,89 @@
+import type { PageDto } from '../../types'
+
+export interface TenantPageDto extends PageDto {
+  paramName?: string
+  paramKey?: string
+}
+
+export interface TenantSubmitDto {
+  /**
+   * 账号额度
+   */
+  accountNumber?: number
+  /**
+   * 联系地址
+   */
+  address?: string
+  /**
+   * 系统背景
+   */
+  backgroundUrl?: string
+  /**
+   * 联系电话
+   */
+  contactNumber?: string
+  /**
+   * 创建部门
+   */
+  createDept?: string
+  /**
+   * 创建时间
+   */
+  createTime?: string
+  /**
+   * 创建人
+   */
+  createUser?: string
+  /**
+   * 数据源ID
+   */
+  datasourceId?: string
+  /**
+   * 域名地址
+   */
+  domainUrl?: string
+  /**
+   * 过期时间
+   */
+  expireTime?: string
+  /**
+   * 主键id
+   */
+  id?: string
+  /**
+   * 是否已删除
+   */
+  isDeleted?: number
+  /**
+   * 授权码
+   */
+  licenseKey?: string
+  /**
+   * 联系人
+   */
+  linkman?: string
+  /**
+   * 产品包ID
+   */
+  packageId?: string
+  /**
+   * 业务状态
+   */
+  status?: number
+  /**
+   * 租户ID
+   */
+  tenantId?: string
+  /**
+   * 租户名称
+   */
+  tenantName?: string
+  /**
+   * 更新时间
+   */
+  updateTime?: string
+  /**
+   * 更新人
+   */
+  updateUser?: string
+}
