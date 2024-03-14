2f52e6bb5b93080e95e27080a637a1729d0430b5
Bruce <recall4056@gmail.com>
2024-03-14 16:15:20 +0800

    style(admin): :lipstick: 格式化

--- apps/admin/src/routes/_base/system/users/route.tsx
+++ apps/admin/src/routes/_base/system/users/route.tsx
@@ -9,9 +9,11 @@ export const Route = createFileRoute('/_base/system/users')({
     title: () => t('SYSTEM.USERS'),
     icon: <MaterialSymbolsManageAccountsRounded />
   },
-  loader: () => Promise.all([
+  loader: async () => {
+    await Promise.all([
       queryClient.ensureQueryData(usersQueryOptions(PageUtils.initParams())),
-      queryClient.ensureQueryData(deptTreeQueryOptions()),
-      queryClient.ensureQueryData(systemDictTreeQueryOptions(SystemDictCode.USER_TYPE))
+      queryClient.ensureQueryData(deptTreeQueryOptions())
     ])
+    queryClient.ensureQueryData(systemDictTreeQueryOptions(SystemDictCode.USER_TYPE))
+  }
 })
